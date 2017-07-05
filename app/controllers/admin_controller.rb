1d586021d17a7fd13d26dc8e79f0aeea4603a5d0
benhayehudi <bengreenberg@gmail.com>
2017-07-05 17:20:31 -0400

    stubbing out administration page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,7 +4,7 @@ class AdminController < ApplicationController
 get "/admin/admin-main" do
   @user = current_user
   if logged_in?
-    if @user.id == "1"
+    if @user.id == 1
       erb :'/admin/admin-main'
     else
       flash[:admin_access_error] = "You must be an administrator to access this page."
@@ -16,7 +16,7 @@ end
 get "/admin/all-users" do
   @user = current_user
   if logged_in?
-    if @user.id == "1"
+    if @user.id == 1
       erb :'/admin/all-users'
     else
       flash[:admin_access_error] = "You must be an administrator to access this page."
