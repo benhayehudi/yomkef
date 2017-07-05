11bc0d59c112362bf4719a8e19abfbf0b20512ae
benhayehudi <bengreenberg@gmail.com>
2017-07-05 17:36:30 -0400

    stubbing out administration page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -25,7 +25,17 @@ get "/admin/all-users" do
   end
 end
 
-
+get "/admin/all-listings" do
+  if logged_in?
+    @user = current_user
+    if @user.username == "rabbiben"
+      erb :'/admin/all-listings'
+    else
+      flash[:admin_access_error] = "You must be an administrator to access this page."
+      redirect to '/index'
+    end
+  end
+end
 
 
 
