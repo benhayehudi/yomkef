2f89748e0249c3807b4d478edbc0f8c2de05c120
benhayehudi <bengreenberg@gmail.com>
2017-07-05 17:14:09 -0400

    stubbing out administration page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,24 @@
 class AdminController < ApplicationController
+  use Rack::Flash
 
 get "/admin/admin-main" do
+  logged_in?
+  @user = current_user
+  if @user.id == "1"
   erb :'/admin/admin-main'
+  else
+    flash[:admin_access_error] = "You must be an administrator to access this page."
+    redirect '/index'
 end
 
 get "/admin/all-users" do
+  logged_in?
+  @user = current_user
+  if @user.id == "1"
   erb :'/admin/all-users'
+else
+  flash[:admin_access_error] = "You must be an administrator to access this page."
+  redirect '/index'
 end
 
 
