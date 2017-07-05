cc4a4e087604e6e9cb617461d3394e1a4c439ebf
benhayehudi <bengreenberg@gmail.com>
2017-07-05 19:10:30 -0400

    added administrative update functionality for users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -64,10 +64,23 @@ if logged_in?
 end
 
 get "/admin/edit-user/:id" do
+  if logged_in?
+    @user = current_user
+    if @user.id == 1
+      user_entry = Listing.find_by_id(params[:id])
+      erb :'/admin/edit-user'
+    end
+  end
+end
+
+post "/admin/edit-user/:id" do
 if logged_in?
   @user = current_user
   if @user.id == 1
-    erb :'/admin/edit-user'
+    user_entry = User.find_by_id(params[:id])
+    user_entry.update_attributes(:name => params["name"], :username => params["username"], :email => params["email"], :facebook_url => params["facebook_url"], :twitter_url => params["twitter_url"], :instagram_url => params["instagram_url"], :about_me => params["about_me"])
+    user_entry.save
+    redirect to "/admin/all-users"
   else
     flash[:admin_access_error] = "You must be an administrator to access this page."
     redirect to '/'
